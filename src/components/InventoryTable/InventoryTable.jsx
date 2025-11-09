ce9e7a78d70f9c040be634af8950e06e6053a763
Anton Krivelo <krivelo2017@mail.ru>
2025-11-09 20:14:25 +0300

    removed rerender inventories to click inventory modal

--- src/components/InventoryTable/InventoryTable.jsx
+++ src/components/InventoryTable/InventoryTable.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, memo } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { Alert, Button, ButtonGroup, Typography, Box } from '@mui/material';
 
@@ -21,159 +21,155 @@ const deleteItems = async ({ inventoryId, userId, removeItemsIds }) => {
   }
 };
 
-const InventoryTable = ({
-  inventory,
-  handleDeleteInventory,
-  withControls = true,
-  token = '',
-  userId = '',
-}) => {
-  const { id, name, user_id, category_name, fields = [], items = [], user_name } = inventory;
+const InventoryTable = memo(
+  ({ inventory, handleDeleteInventory, withControls = true, token = '', userId = '' }) => {
+    const { id, name, user_id, category_name, fields = [], items = [], user_name } = inventory;
+    const columns = fields.map(({ field_name = '' }) => ({
+      field: field_name,
+      headerName: field_name,
+      width: 150,
+    }));
 
-  const columns = fields.map(({ field_name = '' }) => ({
-    field: field_name,
-    headerName: field_name,
-    width: 150,
-  }));
+    const displayValue = withControls ? 'inline-flex' : 'none';
 
-  const displayValue = withControls ? 'inline-flex' : 'none';
+    const [successMsg, setSuccessMsg] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [showModal, setShowModal] = useState(false);
+    const [itemsInventory, setItemsInventory] = useState(items);
+    const [selectedRows, setSelectedRows] = useState([]);
 
-  const [successMsg, setSuccessMsg] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [showModal, setShowModal] = useState(false);
-  const [itemsInventory, setItemsInventory] = useState(items);
-  const [selectedRows, setSelectedRows] = useState([]);
+    const { openConfirm, ConfirmDialog } = useConfirmDialog();
 
-  const { openConfirm, ConfirmDialog } = useConfirmDialog();
+    const rows = itemsInventory.map((item) => ({
+      id: item.id,
+      created_at: item.created_at,
+      ...(item.values || {}),
+    }));
 
-  const rows = itemsInventory.map((item) => ({
-    id: item.id,
-    created_at: item.created_at,
-    ...(item.values || {}),
-  }));
+    const handleItemCreated = (newItem) => {
+      setItemsInventory((prev) => [newItem, ...prev]);
+    };
 
-  const handleItemCreated = (newItem) => {
-    setItemsInventory((prev) => [newItem, ...prev]);
-  };
+    const handleDeleteItem = async () => {
+      try {
+        await deleteItems({
+          removeItemsIds: selectedRows,
+          inventoryId: id,
+          userId,
+          token,
+        });
+        setItemsInventory((prevItems) =>
+          prevItems.filter((item) => !selectedRows.includes(item.id)),
+        );
+        setSelectedRows([]);
+        setSuccessMsg('Items successfully deleted.');
+        setTimeout(() => setSuccessMsg(''), 4000);
+      } catch (e) {
+        setErrorMessage('something gone wrong');
+      }
+    };
 
-  const handleDeleteItem = async () => {
-    try {
-      await deleteItems({
-        removeItemsIds: selectedRows,
-        inventoryId: id,
-        userId,
-        token,
+    const formatDate = (isoString) => {
+      return new Date(isoString).toLocaleString('ru-RU', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
       });
-      setItemsInventory((prevItems) => prevItems.filter((item) => !selectedRows.includes(item.id)));
-      setSelectedRows([]);
-      setSuccessMsg('Items successfully deleted.');
-      setTimeout(() => setSuccessMsg(''), 4000);
-    } catch (e) {
-      setErrorMessage('something gone wrong');
-    }
-  };
+    };
 
-  const formatDate = (isoString) => {
-    return new Date(isoString).toLocaleString('ru-RU', {
-      day: '2-digit',
-      month: '2-digit',
-      year: 'numeric',
-    });
-  };
-
-  return (
-    <>
-      <Button
-        onClick={() =>
-          openConfirm({
-            title: 'Delete Inventory',
-            message: 'Are you sure you want to delete this inventory?',
-            onConfirm: () => handleDeleteInventory({ deleteInventoryId: inventory.id }),
-          })
-        }
-        sx={{ marginBottom: '40px', display: displayValue }}
-        color="error"
-        variant="contained"
-        small
-      >
-        Delete inventory
-      </Button>
-      <Typography>Inventory ID:{id}</Typography>
-      <Box display="flex" gap="15px">
-        {category_name && (
-          <Typography variant="h6" sx={{ mb: 2, textAlign: 'center' }}>
-            {category_name}:
-          </Typography>
-        )}
-        <Typography variant="h6" sx={{ mb: 2, textAlign: 'center' }}>
-          {name}
-        </Typography>
-        <Typography variant="h6" sx={{ mb: 2, textAlign: 'center' }}>
-          Created by: {user_name}
-        </Typography>
-        <Typography variant="h6" sx={{ mb: 2, textAlign: 'center' }}>
-          Created at: {formatDate(inventory.created_at)}
-        </Typography>
-      </Box>
-      <ButtonGroup
-        sx={{ mb: 1, display: displayValue, gap: '5px' }}
-        variant="contained"
-        aria-label="Basic button group"
-      >
-        <Button onClick={() => setShowModal(true)} variant="contained">
-          Add item
-        </Button>
+    return (
+      <>
         <Button
-          disabled={selectedRows.length === 0}
           onClick={() =>
             openConfirm({
-              title: 'Delete Item',
-              message: 'Are you sure you want to delete this items?',
-              onConfirm: handleDeleteItem,
+              title: 'Delete Inventory',
+              message: 'Are you sure you want to delete this inventory?',
+              onConfirm: () => handleDeleteInventory({ deleteInventoryId: inventory.id }),
             })
           }
+          sx={{ marginBottom: '40px', display: displayValue }}
+          color="error"
           variant="contained"
+          small
         >
-          Delete item
+          Delete inventory
         </Button>
-      </ButtonGroup>
-      {successMsg && (
-        <Alert severity="success" sx={{ mb: 2, mt: 2 }}>
-          {successMsg}
-        </Alert>
-      )}
-      {errorMessage && (
-        <Alert severity="error" sx={{ mb: 2, mt: 2 }}>
-          {errorMessage}
-        </Alert>
-      )}
-      {ConfirmDialog}
-      <DataGrid
-        onRowSelectionModelChange={(rows) =>
-          handleRowSelection({
-            rows,
-            setSelectedRows,
-            items: itemsInventory,
-          })
-        }
-        checkboxSelection={withControls}
-        rows={rows}
-        columns={columns}
-        pageSizeOptions={[5]}
-        sx={{ border: 0, height: 300, width: '100%' }}
-      />
+        <Typography>Inventory ID:{id}</Typography>
+        <Box display="flex" gap="15px">
+          {category_name && (
+            <Typography variant="h6" sx={{ mb: 2, textAlign: 'center' }}>
+              {category_name}:
+            </Typography>
+          )}
+          <Typography variant="h6" sx={{ mb: 2, textAlign: 'center' }}>
+            {name}
+          </Typography>
+          <Typography variant="h6" sx={{ mb: 2, textAlign: 'center' }}>
+            Created by: {user_name}
+          </Typography>
+          <Typography variant="h6" sx={{ mb: 2, textAlign: 'center' }}>
+            Created at: {formatDate(inventory.created_at)}
+          </Typography>
+        </Box>
+        <ButtonGroup
+          sx={{ mb: 1, display: displayValue, gap: '5px' }}
+          variant="contained"
+          aria-label="Basic button group"
+        >
+          <Button onClick={() => setShowModal(true)} variant="contained">
+            Add item
+          </Button>
+          <Button
+            disabled={selectedRows.length === 0}
+            onClick={() =>
+              openConfirm({
+                title: 'Delete Item',
+                message: 'Are you sure you want to delete this items?',
+                onConfirm: handleDeleteItem,
+              })
+            }
+            variant="contained"
+          >
+            Delete item
+          </Button>
+        </ButtonGroup>
+        {successMsg && (
+          <Alert severity="success" sx={{ mb: 2, mt: 2 }}>
+            {successMsg}
+          </Alert>
+        )}
+        {errorMessage && (
+          <Alert severity="error" sx={{ mb: 2, mt: 2 }}>
+            {errorMessage}
+          </Alert>
+        )}
+        {ConfirmDialog}
+        <DataGrid
+          onRowSelectionModelChange={(rows) =>
+            handleRowSelection({
+              rows,
+              setSelectedRows,
+              items: itemsInventory,
+            })
+          }
+          checkboxSelection={withControls}
+          rows={rows}
+          columns={columns}
+          pageSizeOptions={[5]}
+          sx={{ border: 0, height: 300, width: '100%' }}
+        />
 
-      <ItemsModal
-        open={showModal}
-        onClose={() => setShowModal(false)}
-        title="Create New Items"
-        submitButtonText="Create"
-        inventory={inventory}
-        userId={user_id}
-        onItemCreated={handleItemCreated}
-      />
-    </>
-  );
-};
-
+        <ItemsModal
+          open={showModal}
+          onClose={() => setShowModal(false)}
+          title="Create New Items"
+          submitButtonText="Create"
+          inventory={inventory}
+          userId={user_id}
+          onItemCreated={handleItemCreated}
+        />
+      </>
+    );
+  },
+);
 export default InventoryTable;

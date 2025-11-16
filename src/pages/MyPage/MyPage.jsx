08eaa725476e36d6aaa72894c428963c91359342
Anton Krivelo <krivelo2017@mail.ru>
2025-11-16 23:35:13 +0300

    added logic salesforce_integration

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,14 +1,31 @@
 import { useAuth } from '../../context/AuthContext';
-import { Container, Typography, Paper, Box, Alert, Button } from '@mui/material';
+import { Container, Typography, Paper, Box, Alert, Button, Card } from '@mui/material';
 import InventoriesSection from '../../components/InventoriesSection/InventoriesSection';
 import { useState } from 'react';
 import SalesforceModal from '../../components/Utils/SalesforceModal';
+import axiosBase from '../../api/axiosBase';
+import { GridCheckCircleIcon } from '@mui/x-data-grid';
+import { AuthButton } from '../../components/Utils/AuthButton';
 
 const MyPage = () => {
   const { user } = useAuth();
   const token = localStorage.getItem('token');
   const userId = localStorage.getItem('userId');
   const [showModal, setShowModal] = useState(false);
+  const [verifyResult, setVerifyResult] = useState(false);
+
+  const handleVerify = async () => {
+    const { accountId, contactId } = user.salesforce_integration;
+
+    const query = new URLSearchParams();
+    if (accountId) query.append('accountId', accountId);
+    if (contactId) query.append('contactId', contactId);
+
+    const res = await axiosBase.get(`/api/salesforce/verify?${query.toString()}`);
+    setVerifyResult(res.data);
+  };
+
+  console.log(user);
 
   return (
     <Container maxWidth="md" sx={{ py: 4 }}>
@@ -32,15 +49,49 @@ const MyPage = () => {
           borderRadius: 2,
         }}
       >
-        <Button
-          onClick={() => setShowModal(true)}
-          disabled={!user}
-          variant="contained"
-          size="small"
-          sx={{ mb: '20px' }}
-        >
-          send data to salesforce
-        </Button>
+        <Card sx={{ p: 2, mt: 3 }}>
+          <Typography variant="h6">Salesforce Integration</Typography>
+          {!user.salesforce_integration ? (
+            <Button
+              onClick={() => setShowModal(true)}
+              disabled={!user}
+              variant="contained"
+              size="small"
+              sx={{ mb: '20px' }}
+            >
+              send data to salesforce
+            </Button>
+          ) : (
+            <Button
+              sx={{ mb: '20px', ml: '20px' }}
+              size="small"
+              variant="contained"
+              onClick={handleVerify}
+            >
+              Verify Data
+            </Button>
+          )}
+
+          {user.salesforce_integration ? (
+            <>
+              <Box display="flex" alignItems="center" gap={1} mt={1}>
+                <GridCheckCircleIcon color="success" />
+                <Typography color="green">You have access to Salesforce</Typography>
+              </Box>
+
+              {verifyResult && <AuthButton data={verifyResult} />}
+            </>
+          ) : (
+            <>
+              <Typography color="text.secondary">
+                User is not connected to Salesforce yet.
+              </Typography>
+
+              <AuthButton />
+            </>
+          )}
+        </Card>
+
         <Typography
           variant="h5"
           component="h2"
@@ -105,6 +156,13 @@ const MyPage = () => {
         <SalesforceModal open={showModal} onClose={() => setShowModal(false)} user={user} />
       </Paper>
       {user && <InventoriesSection token={token} userId={userId} header="My inventories" />}
+      {verifyResult && (
+        <Box mt={2}>
+          <Alert severity="info">Salesforce verification result:</Alert>
+
+          <pre>{JSON.stringify(verifyResult, null, 2)}</pre>
+        </Box>
+      )}
     </Container>
   );
 };

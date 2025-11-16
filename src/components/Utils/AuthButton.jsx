b21dfbfa8f6b3257c9cb016fde2027226c5dc86f
Anton Krivelo <krivelo2017@mail.ru>
2025-11-16 22:51:15 +0300

    added btn url default .env

--- src/components/Utils/AuthButton.jsx
+++ src/components/Utils/AuthButton.jsx
@@ -1,15 +1,15 @@
 import { Button } from '@mui/material';
 
 export const AuthButton = () => {
+  const baseUrl = process.env.REACT_APP_BASE_API_URL;
+
   return (
     <Button
       variant="contained"
       color="primary"
-      onClick={() => window.open('http://localhost:4000/salesforce/auth', '_blank')}
+      onClick={() => window.open(`${baseUrl}/salesforce/auth', '_blank`)}
     >
       Click to Auth
     </Button>
   );
 };
-
-//collect98node-production.up.railway.app
